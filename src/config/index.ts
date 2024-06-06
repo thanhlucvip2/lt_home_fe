7bdfbcb263bc85dc915f408b1945cfad94e10e17
Doan Thanh Luc <lucdt@solazu.com>
2024-06-06 12:01:51 +0700

    config env app

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,1 @@
-// export const API_URL = "http://27.65.227.206:3333/Lt_home/api";
-export const API_URL = "http://158.247.248.222:5000/api/v1";
+export const API_URL = process.env.REACT_APP_API_URL;
